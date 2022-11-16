738fc178de9cddbb92025f30b0b515893e4a763f
Marian <mrmoussa@ualberta.ca>
2022-11-16 17:32:09 +0000

    Implemented a function which takes arrays and returns true/false based on match

---
+++ eqArrays.js
@@ -0,0 +1,29 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ ✅ ✅  Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑 🛑 🛑  Assertion Failed: ${actual} !== ${expected}`);
+  }
+
+};
+
+const eqArrays = function (array1, array2) {
+  if (array1.length === array2.length) {
+    for (var i = 0; i < array2.length; i++) {
+      if (array1[i] !== array2[i]) {
+        return false;
+      }
+    }
+  }
+  else {
+    return false;
+  }
+  return true;
+
+
+}
+// // TEST CODE
+// assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+// assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
+// assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);// => true)
+// assertEqual(eqArrays(eqArrays(["1", "2", "3"], ["1", "2", 3]), false)); // => false
