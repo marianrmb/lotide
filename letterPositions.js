e51f0dbaa6bbc716156fa748a3f7d2269dfcba73
Marian <mrmoussa@ualberta.ca>
2022-11-17 22:03:52 +0000

    new function

---
+++ letterPositions.js
@@ -0,0 +1,40 @@
+const eqArrays = function (array1, array2) {
+  if (array1.length === array2.length) {
+    for (var i = 0; i < array2.length; i++) {
+      if (array1[i] !== array2[i]) {
+        return false;
+      }
+    }
+  }
+  else {
+    return false;
+  }
+  return true;
+
+
+}
+const assertArraysEqual = function (a, b) {
+  if (eqArrays(a, b)) {
+      console.log(`🛑 🛑 🛑  Assertion Failed:[${a}] !== [${b}]`);
+  } else {
+      console.log(`✅ ✅ ✅  Assertion Passed:[${a}] === [${b}]`);
+  }
+};
+
+
+const letterPositions = function (sentence) {
+  const result = {};
+  for (let i = 0; i < sentence.length; i ++){
+    let character = sentence[i];
+    if (result[character]) {
+      result[character].push(i);
+    }else {
+      result[character] = [i];
+    }
+    }
+    
+    return result;
+  }
+  
+  console.log(letterPositions("lighthouse in the house"))
+   
