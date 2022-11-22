2c40d22f9825d91fb18911ea5c231c342cb39d20
Marian <mrmoussa@ualberta.ca>
2022-11-22 04:05:56 +0000

    added module.exports

--- eqArrays.js
+++ eqArrays.js
@@ -1,11 +1,5 @@
-const assertEqual = function (actual, expected) {
-  if (actual === expected) {
-    console.log(`✅ ✅ ✅  Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑 🛑 🛑  Assertion Failed: ${actual} !== ${expected}`);
-  }
+const assertEqual = require('./assertEqual');
 
-};
 
 const eqArrays = function (array1, array2) {
   if (array1.length === array2.length) {
@@ -22,8 +16,4 @@ const eqArrays = function (array1, array2) {
 
 
 }
-// // TEST CODE
-// assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
-// assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
-// assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);// => true)
-// assertEqual(eqArrays(eqArrays(["1", "2", "3"], ["1", "2", 3]), false)); // => false
+module.exports = eqArrays;
